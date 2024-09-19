2bc9e5dfb569217f114cf45fd2a4431544339eb1
g41797 <g41797@gmail.com>
2024-09-19 08:37:43 +0300

    Add description

--- README.md
+++ README.md
@@ -4,6 +4,24 @@
 
 [![CI](https://github.com/g41797/yazq/actions/workflows/ci.yml/badge.svg)](https://github.com/g41797/yazq/actions/workflows/ci.yml)
 
+> Mailboxes are one of the fundamental parts of the [actor model originated in **1973**](https://en.wikipedia.org/wiki/Actor_model). 
+>
+> Through the mailbox mechanism, actors can decouple the reception of a message from its elaboration.
+>
+> An actor is an object that carries out its actions in response to communications it receives.
+>
+> A mailbox is nothing more than the data structure (FIFO) that holds messages.
+>
+> If you send 3 messages to the same actor, it will just execute one at a time.
+
+Useful links for interested:
+- [Huan Mailbox](https://github.com/huan/mailbox)
+- [Typed Mailboxes in Scala](https://www.baeldung.com/scala/typed-mailboxes)
+- [Actors have mailboxes](https://www.brianstorti.com/the-actor-model/)
+
+
+I first encountered MailBox in the late 80s while working om a real-time system: 
+
 > "A **mailbox** is one of two types of objects that can be used for intertask
 communication. When task A wants to send an object to task B, task A
 must send the object to the mailbox, and task B must visit the mailbox,
@@ -14,6 +32,112 @@ needed by the waiting task. On the other hand, the segment might be
 blank, and sending it might constitute a signal to the waiting task.
 Another reason to send an object might be to point out the object to the
 receiving task." 
-> **iRMX 86™ NUCLEUS REFERENCE MANUAL** _Copyright @ 1980, 1981 Intel Corporation_
+> **iRMX 86™ NUCLEUS REFERENCE MANUAL** _Copyright @ 1980, 1981 Intel Corporation.
+
+Since than I have used it in:
+- iRMX      - *PL/M-86*
+- AIX       - *C*
+- Windows   - *C++/C#*
+- Linux     - *Golang*
+
+**Now it's time for Zig**
 
 
+## Example of in-proc Echo "server"
+
+```zig
+    // Mbx is Mailbox with usize letter(data)
+    const Mbx = MailBox(usize);
+
+    // Echo "server" - runs on own thread
+    // Receives letter via 'TO' mailbox
+    // Send letter without change (echo) to "FROM" mailbox
+    // "TO"/"FROM" - from the client point of the view
+    const Echo = struct {
+        const Self = @This();
+
+        to: Mbx = undefined,
+        from: Mbx = undefined,
+        thread: Thread = undefined,
+
+        // Mailboxes creation and start of the thread
+        pub fn start(echo: *Self) void {
+            echo.to = Mbx.open();
+            echo.from = Mbx.open();
+            echo.thread = std.Thread.spawn(.{}, run, .{echo}) catch unreachable;
+        }
+
+        // Thread function
+        fn run(echo: *Self) void {
+            // Main loop:
+            while (true) {
+                // Receive - exit from the thread if mailbox was closed
+                const envelope = echo.to.receive(100000000) catch break;
+                // Send  - exit from the thread if mailbox was closed
+                _ = echo.from.send(envelope) catch break;
+            }
+        }
+
+        // Wait exit from the thread
+        pub fn waitFinish(echo: *Self) void {
+            echo.thread.join();
+        }
+
+        // Close mailboxes
+        // As result Echo "server" should stop processing
+        // and exit from the thread.
+        pub fn stop(echo: *Self) !void {
+            _ = try echo.to.close();
+            _ = try echo.from.close();
+        }
+    };
+
+    // Echo "client"
+    var echo = try std.testing.allocator.create(Echo);
+
+    // Start Echo "server" on own thread
+    echo.start();
+
+    defer {
+        // Wait finish of the thread
+        echo.waitFinish();
+        std.testing.allocator.destroy(echo);
+    }
+
+    // because nothing was send to 'TO' mailbox, nothing should be receiced
+    // from 'FROM' mailbox
+    try testing.expectError(error.Timeout, echo.from.receive(100));
+
+    // Create wrapper for the data
+    const envl = try std.testing.allocator.create(Mbx.Envelope);
+    defer std.testing.allocator.destroy(envl);
+
+    // Send/Receive loop
+    for (0..6) |indx| {
+        // Set value for send [0-5]
+        envl.*.letter = indx;
+
+        // Send to 'TO' mailbox
+        try echo.to.send(envl);
+
+        // Wait received data from OUT mailbox
+        const back = echo.from.receive(1000000);
+
+        if (back) |val| {
+            // Expected value == index [0-5]
+            try testing.expect(val.*.letter == indx);
+        } else |_| {
+            try testing.expect(false);
+        }
+    }
+
+    // Stop Echo "server"
+    try echo.stop();
+}
+//  defered:
+//      Wait finish of the thread
+//          echo.waitFinish();
+//      Free allocated memory:
+//          std.testing.allocator.destroy(echo);
+//          std.testing.allocator.destroy(envl);
+```
--- src/mailbox.zig
+++ src/mailbox.zig
@@ -1,4 +1,6 @@
-//-----------------------------
+// Copyright (c) 2024 g41797
+// SPDX-License-Identifier: MIT
+
 const std = @import("std");
 const builtin = @import("builtin");
 const debug = std.debug;
@@ -8,7 +10,6 @@ const testing = std.testing;
 const Mutex = std.Thread.Mutex;
 const Condition = std.Thread.Condition;
 const Thread = std.Thread;
-//-----------------------------
 
 pub fn MailBox(comptime T: type) type {
     return struct {
@@ -76,6 +77,10 @@ pub fn MailBox(comptime T: type) type {
                 try mbox.cond.timedWait(&mbox.mutex, local_timeout_ns);
             }
 
+            if (mbox.closed) {
+                return error.Closed;
+            }
+
             const first = mbox.dequeue();
 
             if (first) |firstEnvelope| {
@@ -110,6 +115,8 @@ pub fn MailBox(comptime T: type) type {
             const head = mbox.first;
 
             mbox.first = null;
+
+            mbox.cond.signal();
 
             return head;
         }
@@ -163,16 +170,16 @@ test {
 
 //-----------------------------
 test "basic MailBox test" {
-    const Mb = MailBox(u32);
-    var mbox = Mb.open();
+    const Mbx = MailBox(u32);
+    var mbox = Mbx.open();
 
     try testing.expectError(error.Timeout, mbox.receive(10));
 
-    var one = Mb.Envelope{ .letter = 1 };
-    var two = Mb.Envelope{ .letter = 2 };
-    var three = Mb.Envelope{ .letter = 3 };
-    var four = Mb.Envelope{ .letter = 4 };
-    var five = Mb.Envelope{ .letter = 5 };
+    var one = Mbx.Envelope{ .letter = 1 };
+    var two = Mbx.Envelope{ .letter = 2 };
+    var three = Mbx.Envelope{ .letter = 3 };
+    var four = Mbx.Envelope{ .letter = 4 };
+    var five = Mbx.Envelope{ .letter = 5 };
 
     try mbox.send(&one);
     try mbox.send(&two);
@@ -200,67 +207,100 @@ test "basic MailBox test" {
 //-----------------------------
 
 //-----------------------------
-test "mt MailBox test" {
+test "Echo server based on mailboxes test" {
 
-    //-----------------------------
-    const M = MailBox(usize);
+    // Mbx is Mailbox with usize letter(data)
+    const Mbx = MailBox(usize);
 
+    // Echo "server" - runs on own thread
+    // Receives letter via 'TO' mailbox
+    // Send letter without change (echo) to "FROM" mailbox
+    // "TO"/"FROM" - from the client point of the view
     const Echo = struct {
         const Self = @This();
 
-        to: M = undefined,
-        from: M = undefined,
+        to: Mbx = undefined,
+        from: Mbx = undefined,
         thread: Thread = undefined,
 
+        // Mailboxes creation and start of the thread
         pub fn start(echo: *Self) void {
-            echo.to = M.open();
-            echo.from = M.open();
+            echo.to = Mbx.open();
+            echo.from = Mbx.open();
             echo.thread = std.Thread.spawn(.{}, run, .{echo}) catch unreachable;
         }
 
+        // Thread function
         fn run(echo: *Self) void {
+            // Main loop:
             while (true) {
-                const envelope = echo.to.receive(1000000) catch break;
+                // Receive - exit from the thread if mailbox was closed
+                const envelope = echo.to.receive(100000000) catch break;
+                // Send  - exit from the thread if mailbox was closed
                 _ = echo.from.send(envelope) catch break;
             }
         }
 
+        // Wait exit from the thread
         pub fn waitFinish(echo: *Self) void {
             echo.thread.join();
         }
 
+        // Close mailboxes
+        // As result Echo "server" should stop processing
+        // and exit from the thread.
         pub fn stop(echo: *Self) !void {
             _ = try echo.to.close();
             _ = try echo.from.close();
         }
     };
-    //-----------------------------
+
 
     var echo = try std.testing.allocator.create(Echo);
 
+    // Start Echo "server" on own thread
     echo.start();
+
     defer {
+        // Wait finish of the thread
         echo.waitFinish();
         std.testing.allocator.destroy(echo);
     }
 
+    // because nothing was send to 'TO' mailbox, nothing should be receiced
+    // from 'FROM' mailbox
     try testing.expectError(error.Timeout, echo.from.receive(100));
 
-    const envl = try std.testing.allocator.create(M.Envelope);
+    // Create wrapper for the data
+    const envl = try std.testing.allocator.create(Mbx.Envelope);
     defer std.testing.allocator.destroy(envl);
 
+    // Send/Receive loop
     for (0..6) |indx| {
+        // Set value for send [0-5]
         envl.*.letter = indx;
+
+        // Send to 'TO' mailbox
         try echo.to.send(envl);
 
+        // Wait received data from OUT mailbox
         const back = echo.from.receive(1000000);
 
         if (back) |val| {
+            // Expected value == index [0-5]
             try testing.expect(val.*.letter == indx);
         } else |_| {
             try testing.expect(false);
         }
     }
+
+    // Stop Echo "server"
     try echo.stop();
 }
+//  defered:
+//      Wait finish of the thread
+//          echo.waitFinish();
+//      Free allocated memory:
+//          std.testing.allocator.destroy(echo);
+//          std.testing.allocator.destroy(envl);
 //-----------------------------
