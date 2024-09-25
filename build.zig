62a15f129cff36a228bf44e30ed051d58978ef0f
g41797 <g41797@gmail.com>
2024-09-25 12:00:21 +0300

    Separate tests

--- build.zig
+++ build.zig
@@ -33,7 +33,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/mailbox.zig"),
+        .root_source_file = b.path("src/mailbox_tests.zig"),
         .target = target,
         .optimize = optimize,
     });
